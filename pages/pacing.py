e3b39f3d01a2230c27e96dfd6640784f89d2c606
Yutong (Yoki) Qin <yutongq@bu.edu>
2026-03-10 17:48:49 +0000

    suppress warning

--- pages/pacing.py
+++ pages/pacing.py
@@ -1,6 +1,6 @@
 import streamlit as st # type: ignore
 import pandas as pd
-from utils.data_preprocess import load_pacing_data
+from utils.data_preprocess import load_pacing_data # type: ignore
 
 st.set_page_config(page_title="Pacing Analysis", layout="wide")
 st.title("Pacing")
